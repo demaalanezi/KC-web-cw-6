94c3ae645a0cedccf933ee101cc66089122fe1b9
demaalanezi <115086419+demaalanezi@users.noreply.github.com>
2022-10-26 19:49:11 +0300

    done

---
+++ script.js
@@ -0,0 +1,29 @@
+alert('مرحباً بك في موقعي الذي سيحسب درجتك!!');
+
+let grade = prompt("اكتب درجتك بالأرقام");
+
+console.log(prompt);
+
+if (grade >=90) {
+    console.log("لقد حصلت على امتياز 🥳");
+}
+else grade>=80;
+{
+    console.log("لقد حصلت على جيد جداً🤩");
+}
+if (grade >=70);
+{
+    console.log("لقد حصلت على جيد🙂");
+}
+
+if (grade >=60) {
+    console.log("لقد حصلت على مقبول😕");
+}
+else grade >=50;
+{
+    console.log("لقد حصلت على ضعيف☹️");
+}
+
+if (grade<50) {
+    console.log("راسب💔");
+}
